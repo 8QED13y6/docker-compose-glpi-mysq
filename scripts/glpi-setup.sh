270301496985dd298da75971f1207641da536805
8QED13y6 <113854161+8QED13y6@users.noreply.github.com>
2023-09-13 09:39:27 +0200

    Update glpi-setup.sh

--- scripts/glpi-setup.sh
+++ scripts/glpi-setup.sh
@@ -3,8 +3,8 @@
 if [ ! -f "/var/www/html/glpi/config/config_db.php" ]; then
     apt-get update && apt-get install -y wget tar nginx php7.4 php7.4-fpm php7.4-mysql php7.4-xml php7.4-mbstring php7.4-curl php7.4-intl php7.4-gd
 
-    wget https://github.com/glpi-project/glpi/releases/download/9.5.7/glpi-9.5.7.tgz
-    tar -xvf glpi-9.5.7.tgz -C /var/www/html
+    wget https://github.com/glpi-project/glpi/releases/download/10.0.9/glpi-10.0.9.tgz
+    tar -xvf glpi-10.0.9.tgz -C /var/www/html
 
     chown -R www-data:www-data /var/www/html/glpi
 
@@ -19,7 +19,7 @@ if [ ! -f "/var/www/html/glpi/config/config_db.php" ]; then
         --db-user="${glpi_database_user}" \
         --db-password="${glpi_database_password}" \
         --db-name="${glpi_database_name}" \
-        --default-language="en_GB" \
+        --default-language="fr_FR" \
         --no-interaction
 fi
 
